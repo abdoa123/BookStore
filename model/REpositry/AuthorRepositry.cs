abaf0b35791b5ac398c45f521943496363a94952
uncle <Snigga>
2021-03-26 10:39:12 +0200

    add more file and create author model and book model repositries

---
+++ model/REpositry/AuthorRepositry.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.model.REpositry
+{
+    public class AuthorRepositry : IBookStoreRepo<Author>
+    {
+        List<Author> authors;
+        public AuthorRepositry()
+        {
+            authors = new List<Author>()
+            {
+               new Author {Id = 1 , fullName="Abdelrahman" },
+               new Author {Id = 2 , fullName="Abdelrahman2" },
+               new Author {Id = 3 , fullName="Abdelrahman3" },
+                              
+            };
+            
+        }
+        public IList<Author> List()
+        {
+            return authors;
+        }
+
+        public Author find(int id)
+        {
+            return authors.SingleOrDefault(x => x.Id == id);
+        }
+
+        public void Add(Author entity)
+        {
+            authors.Add(entity);
+        }
+
+        public void update(int id, Author entity)
+        {
+            var author = find(id);
+            author.fullName = entity.fullName;
+        }
+
+        public void Delete(int id)
+        {
+            var author = find(id);
+            authors.Remove(author);
+        }
+    }
+}
