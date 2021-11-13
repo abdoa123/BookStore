2ec3204caec21decc881cd4e66cf688a806f41d6
Tony <tony@local.com>
2021-11-13 11:49:11 +0200

    start work with author

---
+++ Bookstore/Controllers/AuthorController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Bookstore.Models.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IBookstoreRepository<Author> authorRepositry;
+
+        public AuthorController(IBookstoreRepository<Author> AuthorRepositry)
+        {
+            this.authorRepositry = AuthorRepositry;
+        }
+        // GET: Author
+        public ActionResult Index()
+        {
+            var authors = authorRepositry.List();
+            return View(authors);
+        }
+
+        // GET: Author/Details/5
+        public ActionResult Details(int id)
+        {
+            var authorId = authorRepositry.Find(id);
+            return View(authorId);
+        }
+
+        // GET: Author/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Author/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Author/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Author/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Author/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Author/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
